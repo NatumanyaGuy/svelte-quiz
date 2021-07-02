5183c86cafa50b8912988ac5074c50ee5fb89cfc
NatumanyaGuy <66813044+NatumanyaGuy@users.noreply.github.com>
2021-07-02 20:44:18 +0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	//import pages
+	import Start from './Auth/Start.svelte'
+	import Signup from './Auth/Signup.svelte'
+	import Home from './Pages/Home.svelte'
+	//import store data
+	import {loggedIn} from './stores.js'
+
+
+
+</script>
+
+
+{#if $loggedIn}
+<Home/>
+
+{:else}
+
+<Signup/>
+{/if}
---
+++ src/Auth/Signup.svelte
@@ -0,0 +1,13 @@
+<script>
+//import store data
+import {loggedIn} from "../stores.js";
+
+function logIn(){
+loggedIn.set(true)
+}
+
+</script>
+
+SignUp 
+
+<button on:click="{logIn}">SignUp</button>
